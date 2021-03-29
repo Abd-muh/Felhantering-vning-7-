a1ab2f92cff9ad6f7fc51ab0d616b1bf4b464562
Abd-muh <62092827+Abd-muh@users.noreply.github.com>
2021-03-29 12:53:36 +0200

    Add files via upload

---
+++ Uppgift PRO/Uppgift PRO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_PRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("Mata in start:");
+			int start = int.Parse(Console.ReadLine());
+			Console.Write("Mata in stop:");
+			int stop = int.Parse(Console.ReadLine());
+			Console.Write("mata in steg:");
+			int steg = int.Parse(Console.ReadLine());
+
+			try
+			{
+
+				for (int i = start; i<= stop; i = i + steg)
+                {
+				Console.WriteLine(i);
+                }
+			}
+			catch (Exception e)
+			{
+				// "default" stället om inget annat matchaar
+			}
+
+
+			Console.ReadKey();
+		}
+    }
+}
